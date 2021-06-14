78499878d828e172030ec216769f2ff17564c3fb
Kelly Jerrell <kcjerrell@gmail.com>
2021-06-14 16:51:17 -0700

    some formatting. changed some test expect() calls to better options

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -22,15 +22,12 @@ test("our test runner works", () => {
     expect(5).toEqual(2 + 3);
 });
 
-//Tasks
-//1. Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
+
 test("renders without error", () => {
     render(<Episode episode={testEpisode} />);
 });
 
-//2. Modify the test data to display a specific summary statement. Complete a test that shows that the summary value
-//   passed in to the Episode component displays as expected.Use no more then 3 different expect statements to test
-//   the the existance of the summary value.
+
 test("renders the summary test passed as prop", async () => {
     const summary = "This is the one with the monster";
     const episode = { ...testEpisode, summary };
@@ -39,18 +36,20 @@ test("renders the summary test passed as prop", async () => {
 
     const element = await screen.findByText(summary);
 
-    expect(element).toBeInTheDocument();
     expect(element).toBeVisible();
 });
 
-//3. The episode component displays a default value ('./stranger_things.png') when a image url is not provided.
-//   Create a new piece of test data with the image property set to null.Test that the alt tag of the image displayed
-//   is set to './stranger_things.png'.
+
 test("renders default image when image is not defined", () => {
     render(<Episode episode={testEpisodeWithoutImage} />);
 
     const image = screen.getByAltText('./stranger_things.png');
 
-    expect(image).toBeInTheDocument();
     expect(image).toBeVisible();
 });
+
+
+//Tasks
+//1. Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
+//2. Modify the test data to display a specific summary statement. Complete a test that shows that the summary value passed in to the Episode component displays as expected.Use no more then 3 different expect statements to test the the existance of the summary value.
+//3. The episode component displays a default value ('./stranger_things.png') when a image url is not provided. Create a new piece of test data with the image property set to null.Test that the alt tag of the image displayed is set to './stranger_things.png'.
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -3,13 +3,9 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Show from './../Show';
-
 import { testEpisode } from './Episode.test';
 
-//Tasks:
-//1. Build an example data structure that contains the show data in the correct format. A show should contain a name,
-//   a summary and an array of seasons, each with a id, name and(empty) list of episodes within them.Use console.logs
-//   within the client code if you need to to verify the structure of show data.
+
 export const testShow = {
     name: "A Real Show",
     image: "probablyNotARealPicture.jpg",
@@ -31,54 +27,47 @@ export const testShow = {
     }]
 }
 
-//2. Test that the Show component renders when your test data is passed in through show and "none" is passed in
-//   through selectedSeason.
+
 test('renders testShow and no selected Season without errors', () => {
     render(<Show show={testShow} selectedSeason="none" />);
 });
 
-//3. Test that the Loading component displays when null is passed into the show prop (look at the Loading component
-//   to see how to test for it's existance)
+
 test('renders Loading component when prop show is null', () => {
     render(<Show show={null} />);
 
     const loading = screen.getByTestId("loading-container");
 
-    expect(loading).toBeInTheDocument();
     expect(loading).toBeVisible();
 });
 
-//4. Test that when your test data is passed through the show prop, the same number of season select options appears
-//   as there are seasons in your test data.
+
 test('renders same number of options seasons are passed in', () => {
     render(<Show show={testShow} selectedSeason="none" />);
 
     const options = screen.getByLabelText(/select a season/i);
-    expect(options).toBeInTheDocument();
     expect(options).toBeVisible();
-
-    expect(options.options.length).toEqual(testShow.seasons.length + 1);
+    expect(options).toHaveLength(testShow.seasons.length + 1);
 });
 
-//5. Test that when an item is selected, the handleSelect function is called. Look at your code to see how to get
-//   access to the select Dom element and userEvent reference materials to see how to trigger a selection.
+
 test('handleSelect is called when an season is selected', () => {
     const mockHandleSelect = jest.fn();
 
     render(<Show show={testShow} selectedSeason="none" handleSelect={mockHandleSelect} />);
 
     const options = screen.getByLabelText(/select a season/i);
-    expect(options).toBeInTheDocument();
     expect(options).toBeVisible();
-    expect(options.options.length).toEqual(testShow.seasons.length + 1);
+    expect(options).toHaveLength(testShow.seasons.length + 1);
+
+    expect(mockHandleSelect).not.toHaveBeenCalled();
 
     userEvent.selectOptions(options, options.options[1]);
 
-    expect(mockHandleSelect.mock.calls.length).toBeGreaterThan(0);
+    expect(mockHandleSelect).toHaveBeenCalled();
 });
 
-//6. Test that the episode component DOES NOT render when the selectedSeason props is "none" and DOES render the
-//    episode component when the selectedSeason prop has a valid season index.
+
 test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
     const { rerender } = render(<Show show={testShow} selectedSeason="none" />);
 
@@ -88,7 +77,13 @@ test('component renders when no seasons are selected and when rerenders with a s
     rerender(<Show show={testShow} selectedSeason={1} />)
 
     const episodesB = screen.queryByTestId('episodes-container');
-    expect(episodesB).toBeInTheDocument();
     expect(episodesB).toBeVisible();
-
 });
+
+//Tasks:
+//1. Build an example data structure that contains the show data in the correct format. A show should contain a name, a summary and an array of seasons, each with a id, name and(empty) list of episodes within them.Use console.logs within the client code if you need to to verify the structure of show data.
+//2. Test that the Show component renders when your test data is passed in through show and "none" is passed in through selectedSeason.
+//3. Test that the Loading component displays when null is passed into the show prop (look at the Loading component to see how to test for it's existance)
+//4. Test that when your test data is passed through the show prop, the same number of season select options appears as there are seasons in your test data.
+//5. Test that when an item is selected, the handleSelect function is called. Look at your code to see how to get access to the select Dom element and userEvent reference materials to see how to trigger a selection.
+//6. Test that the episode component DOES NOT render when the selectedSeason props is "none" and DOES render the episode component when the selectedSeason prop has a valid season index.
